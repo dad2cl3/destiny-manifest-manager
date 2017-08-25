dd346487b2125772d754abed38d53ddf2d1d4a82
Jason M. Chalmers <jason.m.chalmers@gmail.com>
2017-08-24 20:32:22 -0400

    Removing PHP solution

--- js/create_table-t_manifest_version.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS manifest.t_manifest_version;
-
-CREATE TABLE manifest.t_manifest_version (
-	manifest_id SERIAL,
-	effective_date DATE DEFAULT CURRENT_DATE,
-	version VARCHAR(50),
-	path VARCHAR(100),
-	current BOOLEAN DEFAULT TRUE
-);

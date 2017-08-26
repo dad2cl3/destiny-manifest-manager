6fcce9126ff2400f31bb10338fb8ca69cedcd655
Jason M. Chalmers <jason.m.chalmers@gmail.com>
2017-08-25 20:07:17 -0400

    Changed sample query

--- sql/examples.sql
+++ sql/examples.sql
@@ -16,7 +16,8 @@ WITH stats AS (
 modes AS (
 	SELECT json->>'modeType' game_mode, json->>'modeName' mode_name
 	FROM manifest.t_manifest WHERE table_name = 'DestinyActivityModeDefinition'
-	/*AND json->>'modeName' = 'Rumble'*/)
+	AND json @> ('{ "modeName" : "Rumble" }')::jsonb
+)
 SELECT stats.stat_name, modes.mode_name
 FROM stats, modes
 WHERE stats.game_mode = modes.game_mode

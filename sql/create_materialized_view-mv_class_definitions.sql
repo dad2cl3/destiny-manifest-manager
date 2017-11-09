9613d6ca9fe421623b8515fd5ab8f1683a4d6787
Jason M. Chalmers <jason.m.chalmers@gmail.com>
2017-11-09 07:15:44 -0500

    Small changes to several materialized views

--- sql/create_materialized_view-mv_class_definitions.sql
+++ sql/create_materialized_view-mv_class_definitions.sql
@@ -1,12 +1,16 @@
+DROP MATERIALIZED VIEW IF EXISTS manifest.mv_class_definitions;
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS manifest.mv_class_definitions AS
 	SELECT
-		dcd.json->>'hash' class_hash,
+		(dcd.json->>'hash')::BIGINT class_hash,
 		dcd.json->>'classType' class_type,
 		dcd.json->'displayProperties'->>'name' class_name
 	FROM manifest.t_manifest dcd
 	WHERE dcd.deleted IS NULL
 	AND dcd.table_name = 'DestinyClassDefinition'
-	ORDER BY class_hash
+	ORDER BY class_type
 WITH DATA;
 
-CREATE INDEX idx_class_definition_class_hash ON mv_class_definitions USING btree (class_hash);
+-- DROP INDEX idx_class_definition_class_hash;
+
+CREATE INDEX idx_class_definition_class_hash ON manifest.mv_class_definitions USING btree (class_hash);
